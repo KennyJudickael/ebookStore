9103723a88495f989481c318d426607ac5fe611a
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-05-20 15:32:28 +0300

    zod validator

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import { log } from "console";
 import express from "express";
 import authRouter from "./routes/auth";
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   req.on("data", (chunk) => {
+//     req.body = JSON.parse(chunk);
+//     next();
+//   });
+// });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/auth", authRouter);
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  res.json({});
+});
 
 const port = process.env.PORT || 8989;
 
-app.post("/auth/generate-link", (req, res) => {});
-
 app.listen(port, () => {
-  log(`Server is running on port http://localhost:${port}`);
+  console.log(`The application is running on port http://localhost:${port}`);
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,13 @@
+import { generateAuthLink } from "@/controllers/auth";
+import { emailValidationSchema, validate } from "@/middlewares/validator";
 import { Router } from "express";
 
 const authRouter = Router();
 
-authRouter.post("/generate-link", (req, res) => {});
+authRouter.post(
+  "/generate-link",
+  validate(emailValidationSchema),
+  generateAuthLink
+);
 
 export default authRouter;
